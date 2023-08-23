35b212c963206d8178556618a88047bd254634ea
Kazimir <kazimirS46@yandex.ru>
2023-08-23 20:20:46 +0400

    Create News section

--- components/News/index.tsx
+++ components/News/index.tsx
@@ -1,11 +1,51 @@
 'use client';
 
+import Image from 'next/image';
+import React from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation } from 'swiper/modules';
+import newsButton from '../../public/button/newsButton.svg';
+import news from './news.json';
+import image1 from '../../public/image/News1.png';
+import 'swiper/css';
+import 'swiper/css/navigation';
 import styles from './index.module.css';
+
+interface INews {
+  id: number;
+  mainImage: {
+    src: string;
+    alt: string;
+  };
+  title: string;
+  description: string;
+}
 
 export function News() {
   return (
     <div className={styles.wrapper}>
-      <div className={styles.slider}></div>
+      <div className={styles.container}>
+        <Swiper navigation={true} modules={[Navigation]} className='mySwiper'>
+          {news.map((news: INews) => (
+            <SwiperSlide key={news.id}>
+              <div className={styles.slide}>
+                <Image
+                  // src={news.mainImage.src}
+                  src={image1}
+                  width={440}
+                  height={264}
+                  alt={news.mainImage.alt}
+                />
+                <article className={styles.article}>
+                  <h2>{news.title}</h2>
+                  <p>{news.description}</p>
+                  <Image src={newsButton} width={440} alt='button' />
+                </article>
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
     </div>
   );
 }
