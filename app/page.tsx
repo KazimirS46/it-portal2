e88f738435ad6b22edf37c79ef4eb3c47989b422
Kazimir <kazimirS46@yandex.ru>
2023-08-23 18:36:42 +0400

    --

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import { Hero } from '@/components/Hero';
 import styles from './main.module.css';
 import { ActionBtn } from '@/components/ActionBtn';
+import { News } from '@/components/News';
 
 export default function Home() {
   return (
@@ -13,7 +14,9 @@ export default function Home() {
           </div>
         </div>
       </section>
-      <section className={styles.news}></section>
+      <section className={styles.news}>
+        <News />
+      </section>
       <section className={styles.solution}></section>
       <section className={styles.devekjgthc}></section>
       <section className={styles.services}></section>
---
+++ components/News/index.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import styles from './index.module.css';
+
+export function News() {
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.slider}></div>
+    </div>
+  );
+}
