94a4b4ff82a0e870938111d6233fcc56aa3e23c2
Kazimir <kazimirS46@yandex.ru>
2023-08-23 16:01:45 +0400

    Page with header

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,19 @@ export default function Home() {
   return (
     <main>
       <div className={styles.wrapper}>
-        <h2>Rjyntyn</h2>
+        <h2>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Autem cum
+          suscipit pariatur, porro architecto dolorum optio aliquid, nobis
+          laudantium enim quibusdam sint dolore ipsum animi eos libero
+          consequatur, voluptas iusto. Sunt mollitia veritatis deleniti quia
+          labore fuga eos est ea enim recusandae modi unde aperiam laudantium
+          aliquam, veniam consequatur ad ex placeat, culpa maiores qui nulla.
+          Excepturi ipsam labore autem reprehenderit maxime officia repellat
+          omnis animi asperiores, eligendi error consectetur libero tempore
+          magni veniam minima distinctio, velit quidem? Modi tempora animi
+          blanditiis debitis fuga explicabo quidem cumque rerum quas, temporibus
+          impedit tenetur, nobis odit iure maiores, perferendis eum esse? Qui.
+        </h2>
       </div>
     </main>
   );
