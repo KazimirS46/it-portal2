4f095499d6496a807bf5c3e6b9a74eb24c974311
Kazimir <kazimirS46@yandex.ru>
2023-09-18 17:44:02 +0400

    add demo pages and fix z-index

---
+++ app/companies/page.tsx
@@ -0,0 +1,28 @@
+import { Metadata } from 'next';
+import styles from './index.module.css';
+
+export const metadata: Metadata = {
+  title: 'Компании',
+  description: 'IT портал самарской области',
+};
+
+export default function Events() {
+  return (
+    <div className={styles.wrapper}>
+      <h1>Компании</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus
+        aperiam debitis voluptatum ex eius recusandae distinctio cum ratione
+        inventore, explicabo eveniet. Laboriosam cupiditate blanditiis non, in
+        labore porro quos dolorem ad minus dolore magnam provident tempore saepe
+        sapiente accusantium omnis odit iusto neque, autem veritatis harum
+        molestias illum hic. At, ipsa deleniti pariatur veniam repellendus
+        laboriosam ex! Iste blanditiis impedit nobis inventore asperiores quod
+        delectus accusantium eveniet, repellendus nemo illo voluptatibus amet
+        officiis, iure vero voluptatum praesentium libero earum! Error placeat
+        aperiam iste enim ullam quaerat repudiandae eaque explicabo dolor odio
+        suscipit possimus quae aspernatur laborum culpa rem, quidem deserunt!
+      </p>
+    </div>
+  );
+}
