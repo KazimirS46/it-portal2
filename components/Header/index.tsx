fdc47fe2f99b63e66c7528ee4e06bd9a831a5dd1
Kazimir <kazimirS46@yandex.ru>
2023-10-06 12:38:45 +0400

    delete old header

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -4,9 +4,6 @@ import { createContext, useState } from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
 import styles from './index.module.css';
-import MenuButton from './components/menuButton';
-import Navigation from './components/Navigation';
-import NavOverlay from './components/NavOverlay';
 
 export const MenuToggleContext = createContext({});
 
@@ -19,25 +16,17 @@ export function Header() {
   return (
     <header>
       <div className={styles.container}>
-        <div className={styles.controlBox}>
-          <Link href={'/'} className={styles.logoLink}>
-            <Image
-              alt=''
-              src={'/logo/mainLogo.png'}
-              priority={false}
-              width={41}
-              height={45}
-              className={styles.mainLogo}
-            />
-          </Link>
-          <MenuButton navOpen={navIsOpen} toggle={navMenuToggle} />
-        </div>
-
-        <MenuToggleContext.Provider value={{ navIsOpen, setIsOpen }}>
-          <Navigation />
-        </MenuToggleContext.Provider>
+        <Link href={'/'} className={styles.logoLink}>
+          <Image
+            alt=''
+            src={'/logo/mainLogo.png'}
+            priority={false}
+            width={41}
+            height={45}
+            className={styles.mainLogo}
+          />
+        </Link>
       </div>
-      {navIsOpen && <NavOverlay />}
     </header>
   );
 }
