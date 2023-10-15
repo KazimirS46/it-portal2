14c478968c10ffcb57556859b91398c44a62f816
Kazimir <kazimirS46@yandex.ru>
2023-10-15 20:23:42 +0400

    The menu response to swipe has been created

--- components/Header/components/NavMobile/components/DropdownMenuButton/index.tsx
+++ components/Header/components/NavMobile/components/DropdownMenuButton/index.tsx
@@ -3,19 +3,13 @@ import styles from './index.module.css';
 interface IProps {
   isOpen: boolean;
   toggle: () => void;
-  overlayToggle: () => void;
 }
 
-export const DropdownMenuButton = ({
-  isOpen,
-  toggle,
-  overlayToggle,
-}: IProps) => {
+export const DropdownMenuButton = ({ isOpen, toggle }: IProps) => {
   let openButton = isOpen && styles.buttonOpen;
 
   const onClickBtn = () => {
     toggle();
-    overlayToggle();
   };
 
   return (
--- components/Header/components/NavMobile/index.tsx
+++ components/Header/components/NavMobile/index.tsx
@@ -1,6 +1,6 @@
 import Link from 'next/link';
 import Image from 'next/image';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { DropdownMenuButton } from './components/DropdownMenuButton';
 import { MobileNavBar } from './components/MobileNavBar';
 import { NavItemType } from '@/types/types';
@@ -8,17 +8,65 @@ import styles from './index.module.css';
 
 interface IProps {
   props: NavItemType[];
-  toggle: () => void;
+  overOpen: () => void;
+  overClose: () => void;
 }
 
-export const NavMobile = ({ props, toggle }: IProps) => {
+export const NavMobile = ({ props, overClose, overOpen }: IProps) => {
   const [navIsOpen, setIsOpen] = useState<boolean>(false);
+  const navItem = props;
 
   const navMenuToggle = () => {
-    setIsOpen(!navIsOpen);
+    setIsOpen((prev) => !prev);
   };
 
-  const navItem = props;
+  let startTouchY: number = 0;
+  let endTouchY: number = 0;
+  let startTouchX: number = 0;
+  let endTouchX: number = 0;
+
+  const touchStart = (event: TouchEvent) => {
+    startTouchY = event.changedTouches[0].pageY;
+    startTouchX = event.changedTouches[0].pageX;
+  };
+
+  const touchEnd = (event: TouchEvent) => {
+    endTouchY = event.changedTouches[0].pageY;
+    endTouchX = event.changedTouches[0].pageX;
+
+    if (
+      startTouchY < 200 &&
+      Math.abs(endTouchX - startTouchX) < 40 &&
+      endTouchY > startTouchY
+    ) {
+      setIsOpen((prev) => !prev);
+    }
+
+    if (
+      startTouchY < 450 &&
+      Math.abs(endTouchX - startTouchX) < 60 &&
+      endTouchY < startTouchY
+    ) {
+      setIsOpen((prev) => !prev);
+    }
+  };
+
+  useEffect(() => {
+    navIsOpen && overOpen();
+    !navIsOpen && overClose();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [navIsOpen]);
+
+  useEffect(() => {
+    document.addEventListener('touchstart', touchStart);
+    document.addEventListener('touchend', touchEnd);
+
+    return () => {
+      document.removeEventListener('touchstart', touchStart);
+      document.removeEventListener('touchend', touchEnd);
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
     <>
@@ -34,15 +82,11 @@ export const NavMobile = ({ props, toggle }: IProps) => {
           />
         </Link>
 
-        <DropdownMenuButton
-          isOpen={navIsOpen}
-          toggle={navMenuToggle}
-          overlayToggle={toggle}
-        />
+        <DropdownMenuButton isOpen={navIsOpen} toggle={navMenuToggle} />
       </div>
 
       <div className={styles.dropdownMenu}>
-        <MobileNavBar props={{ navItem, navIsOpen, navMenuToggle, toggle }} />
+        <MobileNavBar props={{ navItem, navIsOpen, navMenuToggle }} />
       </div>
     </>
   );
