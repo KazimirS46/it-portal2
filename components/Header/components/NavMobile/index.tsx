5185dcddf22ba066ee41cf0af75a5bf5c1dfd72b
Kazimir <kazimirS46@yandex.ru>
2023-10-16 13:34:27 +0400

    delete swipe

--- components/Header/components/NavMobile/index.tsx
+++ components/Header/components/NavMobile/index.tsx
@@ -20,53 +20,11 @@ export const NavMobile = ({ props, overClose, overOpen }: IProps) => {
     setIsOpen((prev) => !prev);
   };
 
-  let startTouchY: number = 0;
-  let endTouchY: number = 0;
-  let startTouchX: number = 0;
-  let endTouchX: number = 0;
-
-  const touchStart = (event: TouchEvent) => {
-    startTouchY = event.changedTouches[0].pageY;
-    startTouchX = event.changedTouches[0].pageX;
-  };
-
-  const touchEnd = (event: TouchEvent) => {
-    endTouchY = event.changedTouches[0].pageY;
-    endTouchX = event.changedTouches[0].pageX;
-
-    if (
-      startTouchY < 200 &&
-      Math.abs(endTouchX - startTouchX) < 40 &&
-      endTouchY > startTouchY
-    ) {
-      setIsOpen((prev) => !prev);
-    }
-
-    if (
-      startTouchY < 450 &&
-      Math.abs(endTouchX - startTouchX) < 60 &&
-      endTouchY < startTouchY
-    ) {
-      setIsOpen((prev) => !prev);
-    }
-  };
-
   useEffect(() => {
     navIsOpen && overOpen();
     !navIsOpen && overClose();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [navIsOpen]);
-
-  useEffect(() => {
-    document.addEventListener('touchstart', touchStart);
-    document.addEventListener('touchend', touchEnd);
-
-    return () => {
-      document.removeEventListener('touchstart', touchStart);
-      document.removeEventListener('touchend', touchEnd);
-    };
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
 
   return (
     <>
